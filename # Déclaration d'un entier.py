2ef54854dd57c6ed07e9dd0e3e716be011d4e763
Dylan-Dufour <dylan.dufour@laplateforme.io>
2024-10-22 16:20:51 +0200

    mon premier coomit

---
+++ # Déclaration d'un entier.py
@@ -0,0 +1,11 @@
+# Déclaration d'un entier
+mon_entier = 42
+
+# Vous pouvez aussi déclarer d'autres entiers
+autre_entier = -7
+nombre_zero = 0
+
+# Affichage des valeurs
+print(mon_entier)
+print(autre_entier)
+print(nombre_zero)
